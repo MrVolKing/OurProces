9b1c97e27739b9154a46f763351bc1f28c0441a3
mrKing <MrKing>
2019-02-21 16:18:00 +0200

    work site

--- src/scripts/common/custom.js
+++ src/scripts/common/custom.js
@@ -1,62 +1,46 @@
 $(document).ready(() => {
 
-    // scroll menu
-    $(document).on('click', '.sectmenu__item a', function() {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({
-            scrollTop: top
-        }, 500);
-    })
-
-    // burger
-    $('.burger').click(function() {
-        $('.adaptmenu').addClass('adaptmenu_active');
-        $('body').css('overflow', 'hidden');
-    });
-
-    $('.close').click(function() {
-        $('.adaptmenu').removeClass('adaptmenu_active');
-        $('body').css('overflow', 'visible');
-        $('body').css('overflow-x', 'hidden');
-    });
-
-    $('.adaptmenu nav').click(function() {
-        $('.adaptmenu').removeClass('adaptmenu_active');
-        $('body').css('overflow', 'visible');
-        $('body').css('overflow-x', 'hidden');
-    });
-
-
-   
-    
     // popap
 
-    $('.show_popap').click(function() {
+    $('.boottomcontent__button button').click(function() {
         $('.modalDialogcustom').addClass('modalDialogcustom_active');
-        $('body').css('overflow', 'hidden')
+        $('.boottomcontent').addClass('content_hidden');
+        $('.boottomcontent__bottom').addClass('content_hidden');
+        $('.footer').addClass('content_hidden');
     });
 
-    $('.contentpopap__cancel').click(function() {
+    $('.contentpopap__close').click(function() {
         $('.modalDialogcustom').removeClass('modalDialogcustom_active');
-        $('body').css('overflow', 'visible')
+        $('.boottomcontent').removeClass('content_hidden');
+        $('.boottomcontent__bottom').removeClass('content_hidden');
+        $('.footer').removeClass('content_hidden');
     });
 
-    $('.shadow_popap').click(function() {
-        $('.modalDialogcustom').removeClass('modalDialogcustom_active');
-        $('body').css('overflow', 'visible')
+    
+    // scrill top
+    $(function(){
+        $('.arr_top').click(function(){
+            $('html,body').animate({scrollTop: 0}, 1000);
+        });
+      });
+
+    
+
+    // validations
+    $('.form__button button').click(function(e) {
+        e.preventDefault();
+        console.log($('.i').val.length )
+        if ($('.i').val.length  < 3) {
+            this.$('.i').addClass('error');
+        }
     });
-
-
     
    
 
 
-
+    // slider
     var menu = ['Process Name', 'Process Name', 'Process Name', 'Process Name', 'Process Name'];
    
-
     var swiper = new Swiper('.swiper-container', {
         loop: true,
         slidesPerView: 1,
